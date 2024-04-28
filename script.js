cd5c73bbbe0747389d5897c57d8a080c99c20695
Ivan Chen <144068027+duhrice@users.noreply.github.com>
2024-04-27 22:38:47 -0400

    Added buttons in index.html for player to use. Reworked playRound function to use template strings and reworked game logic to use the DOM instead of console. Removed functions that were no longer useful.

--- script.js
+++ script.js
@@ -1,77 +1,95 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 10) % 3;
     if (choice == 0) {
-        return 'Rock';
+        return "Rock";
     } else if (choice == 1) {
-        return 'Paper';
+        return "Paper";
     } else {
-        return 'Scissors';
-    }
-}
-
-function getHumanChoice() {
-    let choice = prompt("Please choose rock, paper, or scissors: ");
-    if (choice.toLowerCase() == 'rock' || choice.toLowerCase() == 'paper' || choice.toLowerCase() == 'scissors') {
-        return choice.charAt(0).toUpperCase() + choice.slice(1).toLowerCase();
-    } else {
-        return 'Choice was not one of the options choosen.';
+        return "Scissors";
     }
 }
 
 function playRound(humanChoice, computerChoice) {
-    if (humanChoice == 'Rock') {
-        if (computerChoice == 'Paper') {
-            console.log('Player chose ' + humanChoice + '. Computer chose ' + computerChoice + '. Computer wins!');
+    if (humanChoice == "Rock") {
+        if (computerChoice == "Paper") {
+            roundResult.textContent = `Player chose ${humanChoice}. Computer chose ${computerChoice}. Computer wins!`;
             computerScore++;
-        } else if (computerChoice == 'Scissors') {
-            console.log('Player chose ' + humanChoice + '. Computer chose ' + computerChoice + '. Player wins!');
+        } else if (computerChoice == "Scissors") {
+            roundResult.textContent = `Player chose ${humanChoice}. Computer chose ${computerChoice}. Player wins!`;
             humanScore++;
         } else {
-            console.log('Player chose ' + humanChoice + '. Computer chose ' + computerChoice + '. Tie!');
+            roundResult.textContent = `Player chose ${humanChoice}. Computer chose ${computerChoice}. Tie!`;
         }
-    } else if (humanChoice == 'Paper') {
-        if (computerChoice == 'Scissors') {
-            console.log('Player chose ' + humanChoice + '. Computer chose ' + computerChoice + '. Computer wins!');
+    } else if (humanChoice == "Paper") {
+        if (computerChoice == "Scissors") {
+            roundResult.textContent = `Player chose ${humanChoice}. Computer chose ${computerChoice}. Computer wins!`;
             computerScore++;
-        } else if (computerChoice == 'Rock') {
-            console.log('Player chose ' + humanChoice + '. Computer chose ' + computerChoice + '. Player wins!');
+        } else if (computerChoice == "Rock") {
+            roundResult.textContent = `Player chose ${humanChoice}. Computer chose ${computerChoice}. Player wins!`;
             humanScore++;
         } else {
-            console.log('Player chose ' + humanChoice + '. Computer chose ' + computerChoice + '. Tie!');
+            roundResult.textContent = `Player chose ${humanChoice}. Computer chose ${computerChoice}. Tie!`;
         }
-    } else if (humanChoice == 'Scissors') {
-        if (computerChoice == 'Rock') {
-            console.log('Player chose ' + humanChoice + '. Computer chose ' + computerChoice + '. Computer wins!');
+    } else if (humanChoice == "Scissors") {
+        if (computerChoice == "Rock") {
+            roundResult.textContent = `Player chose ${humanChoice}. Computer chose ${computerChoice}. Computer wins!`;
             computerScore++;
-        } else if (computerChoice == 'Paper') {
-            console.log('Player chose ' + humanChoice + '. Computer chose ' + computerChoice + '. Player wins!');
+        } else if (computerChoice == "Paper") {
+            roundResult.textContent = `Player chose ${humanChoice}. Computer chose ${computerChoice}. Player wins!`;
             humanScore++;
         } else {
-            console.log('Player chose ' + humanChoice + '. Computer chose ' + computerChoice + '. Tie!');
+            roundResult.textContent = `Player chose ${humanChoice}. Computer chose ${computerChoice}. Tie!`;
         }
     } else {
-        console.log('An error occured.');
-    }
-}
-
-function playGame() {
-
-    for (let i = 0; i < 5; i++) {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-
-    if (computerScore > humanScore) {
-        console.log('Computer score is ' + computerScore + '. Player score is ' + humanScore + '. Winner is computer!');
-    } else if (computerScore < humanScore) {
-        console.log('Computer score is ' + computerScore + '. Player score is ' + humanScore + '. Winner is player!');
-    } else {
-        console.log('Computer score is ' + computerScore + '. Player score is ' + humanScore + '. No winner!');
+        roundResult.textContent = "An error occured.";
     }
 }
 
 let computerScore = 0;
 let humanScore = 0;
 
-playGame();
+const results = document.querySelector("#results");
+
+const roundResult = document.createElement("div");
+roundResult.classList.add("result");
+roundResult.textContent = "Let's play Rock, Paper, Scissors! First to 5 wins. \
+                            Choose an option above to start the game.";
+results.appendChild(roundResult);
+
+const score = document.createElement("div");
+score.classList.add("score");
+score.setAttribute("style", "white-space: pre;");
+score.textContent = `Player Score: ${humanScore}\nComputer Score: ${computerScore}`;
+results.appendChild(score);
+
+const winner = document.createElement("div");
+winner.classList.add("winner");
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.id == "Reset") {
+        if (results.contains(winner)) {
+            results.removeChild(winner);
+        }
+        computerScore = 0;
+        humanScore = 0;
+        score.textContent = `Player Score: ${humanScore}\nComputer Score: ${computerScore}`;
+    } else {
+        let computerSelection = getComputerChoice();
+        playRound(button.id, computerSelection);
+
+        score.textContent = `Player Score: ${humanScore}\nComputer Score: ${computerScore}`;
+
+        if (computerScore == 5 || humanScore == 5) {
+            if (computerScore == 5) {
+                winner.textContent = "We have a winner! Computer is the first to reach 5 points!"
+            } else {
+                winner.textContent = "We have a winner! Player is the first to reach 5 points! \
+                                        To start a new game, press the 'Reset' button"
+            }
+            results.appendChild(winner);
+        }
+    }
+  });
+});
